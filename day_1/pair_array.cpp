afb09b9352c274adbd6d09023248cca88ad695b0
Nitin Raj <nitinrana9060888018@gmail.com>
2026-07-14 04:16:01 +0530

    pair_array

---
+++ day_1/pair_array.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+int main(){
+    pair <int,int> arr[]={{1,2},{3,4},{5,6}};
+    cout<<"array third pair second value "<<arr[2].second<<endl;
+    return 0;
+}
