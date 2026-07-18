b564309a17395bd5641e9431fa0c69239fdfe594
Nitin Raj <nitinrana9060888018@gmail.com>
2026-07-18 21:51:11 +0530

    range_based_loop

--- day_1/range_basedForLoop.cpp
+++ day_1/range_basedForLoop.cpp
@@ -10,3 +10,20 @@ int main(){
         cout<<it<<endl;
     }
 }
+
+// WHAT IS Range Based Lopp
+// it was introduced in C++11
+// it use to iterate over every element of container
+// container like vector , array, string, set, map
+// so internally we dont have to write V.begin and v.end it will automatically added by the complier
+
+
+// for example if you Dont use Range based loop
+
+vector <int> v={10,20,30};
+for(auto it =v.begin; it!=v.end; it++){
+cout<<<*it;
+}
+
+// in this see if we use range based loop then the v.begin and v.end and it++
+// will be added internally by the complier
